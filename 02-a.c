239b5cd8e7ba9a0d38758351900a533d81213374
Guilherme Rigon <guilherme@rigon.info>
2016-03-02 01:14:12 -0300

    OK

---
+++ 02-a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int a, b, soma;
+  scanf("%d %d", &a, &b);
+  printf("O Valor da primeira variavel eh: %d\n", a);
+  printf("O Valor da primeira variavel eh: %d\n", b);
+  soma = a + b;
+  printf("O Valor da soma A + B eh %d\n", soma);
+
+}
